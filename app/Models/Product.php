f9e70aca6216b00e1be4b0ea9e74f2c67003e84b
signsofdidier <signsofdidier@gmail.com>
2025-05-13 16:20:07 +0200

    first migrations & models

---
+++ app/Models/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'image',
+        'is_active',
+        'is_featured',
+        'in_stock',
+        'on_sale',
+        'category_id',
+        'brand_id',
+    ];
+
+    // hierdoor worden de images omgezet naar een array uit de JSON van images
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(){
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function orderItems(){
+        return $this->hasMany(OrderItem::class);
+    }
+}

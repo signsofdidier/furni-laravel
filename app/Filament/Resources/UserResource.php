87f1e373312410a3b3e5c28d6f0fba3bab8d062c
signsofdidier <signsofdidier@gmail.com>
2025-05-13 20:41:08 +0200

    user table

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Pages\Page;
+use Filament\Resources\Pages\CreateRecord;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email Address')
+                    ->email()
+                    ->maxLength(255)
+                    // ignoreRecord:true = Negeert het huidige record bij unieke validatie, zodat je geen fout krijgt als de waarde niet is gewijzigd tijdens het bewerken.
+                    ->unique(ignoreRecord: true)
+                    ->required(),
+                DateTimePicker::make('email_verified_at')
+                ->label('Email Verified At')
+                ->default(now()),
+                TextInput::make('password')
+                    ->password()
+                    // hashed het password
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                    // Alleen opslaan als er een waarde is ingevuld
+                    ->dehydrated(fn ($state) => filled($state))
+                    // Alleen verplicht bij het aanmaken, niet bij bewerken
+                    ->required(fn(Page $livewire): bool => $livewire instanceof CreateRecord)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('email')
+                ->searchable(),
+                TextColumn::make('email_verified_at')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}

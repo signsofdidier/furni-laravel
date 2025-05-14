eb886b8936bb13a9ed830de6b5c5188d5db84806
signsofdidier <signsofdidier@gmail.com>
2025-05-14 15:31:40 +0200

    order widgets en tabs gemaakt

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -16,4 +18,26 @@ class ListOrders extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    // widgets bovenaan
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class
+        ];
+    }
+
+    // tabs voor status queries
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All'),
+            'new' => Tab::make('New'),
+            'processing' => Tab::make('Processing')->query(fn ($query) => $query->where('status' , 'processing')),
+            'shipped' => Tab::make('Shipped')->query(fn ($query) => $query->where('status', 'shipped')),
+            'delivered' => Tab::make('Delivered')->query(fn ($query) => $query->where('status', 'delivered')),
+            'cancelled' => Tab::make('Cancelled')->query(fn ($query) => $query->where('status', 'cancelled')),
+        ];
+    }
+
 }
---
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\OrderResource\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
+
+class OrderStats extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            // toon alle orders met status 'new'
+            Stat::make('New Orders', Order::query()->where('status', 'new')->count()),
+            Stat::make('Order Processing', Order::query()->where('status', 'processing')->count()),
+            Stat::make('Order Shipped', Order::query()->where('status', 'shipped')->count()),
+
+            // dit toont de gemiddelde prijs van alle orders
+            Stat::make('Average Price', Number::currency(Order::query()->avg('grand_total'), 'EUR')),
+        ];
+    }
+}

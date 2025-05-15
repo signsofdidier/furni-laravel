34068ab6f210353bbf644f9dd709e78f302d1a5e
signsofdidier <signsofdidier@gmail.com>
2025-05-15 17:26:52 +0200

    home & categories dynamic

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
 use Livewire\Component;
+
+// Titel (tablat)
+#[Title('Home Page - Ecommrce')]
 
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        // als de brand actief is geven we hem weer (is_active staat in de brand model)
+        $brands = Brand::where('is_active', 1)->get();
+        $categories = Category::where('is_active', 1)->get();
+
+
+        return view('livewire.home-page', [
+            'brands' => $brands,
+            'categories' => $categories,
+        ]);
     }
 }

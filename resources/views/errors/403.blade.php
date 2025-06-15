d445700d8379089778734f60d05fd0d2b2a1af5c
signsofdidier <signsofdidier@gmail.com>
2025-06-15 19:32:02 +0200

    403

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,18 @@
-@extends('errors::minimal')
-
-@section('title', __('Forbidden'))
-@section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+<x-layouts.app :hideBreadcrumb="true">
+    <div class="error-page mt-100">
+        <div class="container">
+            <div class="error-content text-center">
+                <h1 class="fw-bold" style="font-size:12rem; line-height:1;">
+                    403
+                </h1>
+                <p class="h4 mb-3">Forbidden</p>
+                <p class="mb-4">
+                    You do not have permission to access this page.
+                </p>
+                <a href="{{ url('/') }}" class="btn btn-primary">
+                    BACK TO HOMEPAGE
+                </a>
+            </div>
+        </div>
+    </div>
+</x-layouts.app>
